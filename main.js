f73956fe643c41d7777838e292e13f4628e98c0e
Robin Steil <robin.steil@gmx.de>
2019-01-13 16:31:40 +0100

    enter und leave events- 4 statt einem Bild - Profilseiten

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function Person(firstName, lastName, age, city, profilePicture, 
     minSize, maxCost,
-    interests, smoking, animals, profileURL) {
+    arrayInterests, smoking, animals, profileURL) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.age = age;
@@ -8,7 +8,7 @@ function Person(firstName, lastName, age, city, profilePicture,
     this.profilePicture = profilePicture;
     this.minSize = minSize;
     this.maxCost = maxCost;
-    this.interests = interests;
+    this.arrayInterests = arrayInterests;
     this.smoking = smoking;
     this.animals = animals;
     this.profileURL= profileURL;
@@ -16,9 +16,13 @@ function Person(firstName, lastName, age, city, profilePicture,
 
 let arrayPersons = [
     new Person("Robin", "Steil", 25, "Essen", "images/profile_robin.jpg", 10, 350,
-    "images/interests_robin.png", true, "keine", "profil_robin.html"),
+    ["images/interests/burger.jpg", "images/interests/musik.jpg", "images/interests/tennis.jpg", "images/interests/got.jpg"], true, "keine", "profil_robin.html"),
     new Person("Hannah", "Nachname", 23, "Duisburg", "images/profile_hannah.jpg", 15, 400,
-    "images/interests_hannah.png", false, "keine", "profil_hannah.html")
+    ["images/interests/eis.jpg", "images/interests/shopping.jpg", "images/interests/surfen.jpg", "images/interests/champagne.jpg"], false, "keine", "profil_hannah.html"),
+    new Person("Emilie", "Nachname", 19, "Duisburg", "images/profile_emilie.jpg", 12, 320,
+    ["images/interests/hantel.jpg", "images/interests/fussball.jpg", "images/interests/kamera.jpg", "images/interests/backpack.jpg"], false, "keine", "#"),
+    new Person("Hannah", "Nachname", 23, "Duisburg", "images/profile_karl.jpg", 15, 400,
+    ["images/interests/fahrrad.jpg", "images/interests/roller.jpg", "images/interests/waffel.jpg", "images/interests/klavier.jpg"], false, "keine", "#")
 ];
 
 function filterFunction(person){
@@ -52,22 +56,69 @@ function createProfileList(arrayPersons) {
     
     $("#container-wg-profiles").append(`<div class="my-5 container-profile" style="display:inline-block; position:relative; width:400px; height:400px;" id="container-profile-${index}"></div>`);
 
-    $(`#container-profile-${index}`).append(`<a style="position:absolute; height:400px; width:400px; display:block;" href="${value.profileURL}"><img id="profilePicture-${index}" src="${value.profilePicture}" style="position:absolute; height:400px; width:400px" alt="Profilbild"></img></a>`);
+    $(`#container-profile-${index}`).append(`<a style="position:absolute; height:400px; width:400px; display:block;" href="${value.profileURL}">
+    <img id="profilePicture-${index}" src="${value.profilePicture}" style="position:absolute; height:400px; width:400px" alt="Profilbild"></img></a>`);
 
     if(value.smoking) {
         $(`#container-profile-${index}`).append(`<img style="position:absolute; bottom:5px; left:20px;" src="images/smoking.jpg" height="30px" width="30px" alt="Raucher"></img>`);
     }
     
-    $(`#container-profile-${index}`).append(`<img style="position:absolute;" class="interests-picture" id="InterestsPicture-${index}" src="${value.interests}" height="400px" width="400px" alt="Interessen" ></img>`);
+    $(`#container-profile-${index}`).append(
+       `<img style="position:absolute; top:0px; left:0px" class="interests-picture" src="${value.arrayInterests[0]}" height="200px" width="200px" alt="Interessen" ></img>`,
+        `<img style="position:absolute; top:0px; left:200px" class="interests-picture" src="${value.arrayInterests[1]}" height="200px" width="200px" alt="Interessen" ></img>`,
+       `<img style="position:absolute; top:200px; left:0px" class="interests-picture" src="${value.arrayInterests[2]}" height="200px" width="200px" alt="Interessen" ></img>`,
+       `<img style="position:absolute; top:200px; left:200px" class="interests-picture" src="${value.arrayInterests[3]}" height="200px" width="200px" alt="Interessen" ></img>`);
     });
 };
 
 $(document).ready(function() {
    
     $("body").on("click",".interests-picture", function() {
-        $(event.target).hide();
+        $(event.target).fadeOut("slow");
     });
 
+    $("body").on("mouseenter",".interests-picture", function() {
+        document.body.style.cursor="pointer";
+        $(event.target).css({zIndex: 10});
+        $("#dark-background").fadeTo(100,0.5);
+    });
+
+    $("body").on("mouseleave",".interests-picture", function() {
+        document.body.style.cursor="default";
+        $(event.target).css({zIndex: 0});
+        $("#dark-background").hide();
+    });
+
+
+
+/*
+    $("body").on("mouseenter",".interests-picture", function() {
+        //console.log(1);
+        $(event.target).css({zIndex: 10});
+        event.stopImmediatePropagation();
+    });
+
+    $("body").on("mouseleave",".interests-picture", function() {
+        //console.log(2);
+        $(event.target).css({zIndex: 0});
+        $("#dark-background").hide();
+        event.stopImmediatePropagation();
+    });
+
+    $("body").on("mouseenter",".container-profile", function() {
+        //alert(3);
+        document.body.style.cursor="pointer";
+        $(event.target).css({zIndex: 100});
+        $("#dark-background").fadeTo(100,0.5);
+    });
+
+    $("body").on("mouseleave",".container-profile", function() {
+        //alert(4);
+        $(event.target).css({zIndex: 100});
+        document.body.style.cursor="default";
+        $("#dark-background").hide();
+    });
+*/
     $("#filter-button").on("click", function(){
         createProfileList(
             arrayPersons.filter(filterFunction))});
