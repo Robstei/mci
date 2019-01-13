fb54f5c0b0ec05a845b18f2df98ecb63c3b93319
Robstei <robin.steil@gmx.de>
2019-01-13 21:12:30 +0100

    finaler schliff

--- main.js
+++ main.js
@@ -16,19 +16,21 @@ function Person(firstName, lastName, age, city, profilePicture,
 
 let arrayPersons = [
     new Person("Robin", "Steil", 25, "Essen", "images/profile_robin.jpg", 10, 350,
-    ["images/interests/burger.jpg", "images/interests/musik.jpg", "images/interests/tennis.jpg", "images/interests/got.jpg"], true, "keine", "profil_robin.html"),
+    ["images/interests/burger.jpg", "images/interests/musik.jpg", "images/interests/tennis.jpg", "images/interests/got.jpg"], true, false, "profil_robin.html"),
     new Person("Hannah", "Nachname", 23, "Duisburg", "images/profile_hannah.jpg", 15, 400,
-    ["images/interests/eis.jpg", "images/interests/shopping.jpg", "images/interests/surfen.jpg", "images/interests/champagne.jpg"], false, "keine", "profil_hannah.html"),
+    ["images/interests/eis.jpg", "images/interests/shopping.jpg", "images/interests/surfen.jpg", "images/interests/champagne.jpg"], false, true, "profil_hannah.html"),
     new Person("Emilie", "Nachname", 19, "Duisburg", "images/profile_emilie.jpg", 12, 320,
-    ["images/interests/hantel.jpg", "images/interests/fussball.jpg", "images/interests/kamera.jpg", "images/interests/backpack.jpg"], false, "keine", "#"),
+    ["images/interests/hantel.jpg", "images/interests/fussball.jpg", "images/interests/kamera.jpg", "images/interests/backpack.jpg"], false, false, "#"),
     new Person("Hannah", "Nachname", 23, "Duisburg", "images/profile_karl.jpg", 15, 400,
-    ["images/interests/fahrrad.jpg", "images/interests/roller.jpg", "images/interests/waffel.jpg", "images/interests/klavier.jpg"], false, "keine", "#")
+    ["images/interests/fahrrad.jpg", "images/interests/roller.jpg", "images/interests/waffel.jpg", "images/interests/klavier.jpg"], false, true, "#")
 ];
 
 function filterFunction(person){
     let city = $("input[name=filter-city]").val();
     let size = $("input[name=filter-size]").val();
     let cost = $("input[name=filter-cost]").val();
+    let notsmoking = $("input[name=filter-smoking]").prop('checked');
+    let noanimals = $("input[name=filter-animal]").prop('checked');
    
     if(city != "") {
         if (city != person.city) {
@@ -42,6 +44,18 @@ function filterFunction(person){
     }
     if(cost != "") {
         if (cost > person.maxCost) {
+            return false;
+        }
+    }
+
+    if(notsmoking) {
+        if (person.smoking) {
+            return false;
+        }
+    }
+
+    if(noanimals) {
+        if (person.animals) {
             return false;
         }
     }
@@ -60,7 +74,16 @@ function createProfileList(arrayPersons) {
     <img id="profilePicture-${index}" src="${value.profilePicture}" style="position:absolute; height:400px; width:400px" alt="Profilbild"></img></a>`);
 
     if(value.smoking) {
-        $(`#container-profile-${index}`).append(`<img style="position:absolute; bottom:5px; left:20px;" src="images/smoking.jpg" height="30px" width="30px" alt="Raucher"></img>`);
+        $(`#container-profile-${index}`).append(`<img style="position:absolute; bottom:5px; left:20px;" src="images/icons/cigarette.png" height="30px" width="30px" alt="Raucher"></img>`);
+    } else {
+        $(`#container-profile-${index}`).append(`<img style="position:absolute; bottom:5px; left:20px;" src="images/icons/cigarette_not.png" height="30px" width="30px" alt="Raucher"></img>`);
+    }
+
+    if(value.animals) {
+        $(`#container-profile-${index}`).append(`<img style="position:absolute; bottom:5px; left:60px;" src="images/icons/tier.png" height="30px" width="30px" alt="Raucher"></img>`);
+    } else {
+        $(`#container-profile-${index}`).append(`<img style="position:absolute; bottom:5px; left:60px;" src="images/icons/tier_not.png" height="30px" width="30px" alt="Raucher"></img>`);
+
     }
     
     $(`#container-profile-${index}`).append(
@@ -76,49 +99,19 @@ $(document).ready(function() {
     $("body").on("click",".interests-picture", function() {
         $(event.target).fadeOut("slow");
     });
-
+    
     $("body").on("mouseenter",".interests-picture", function() {
         document.body.style.cursor="pointer";
-        $(event.target).css({zIndex: 10});
-        $("#dark-background").fadeTo(100,0.5);
+       // $(event.target).css({zIndex: 10});
+        //$("#dark-background").fadeTo(100,0.5);
     });
 
     $("body").on("mouseleave",".interests-picture", function() {
         document.body.style.cursor="default";
-        $(event.target).css({zIndex: 0});
-        $("#dark-background").hide();
+        //$(event.target).css({zIndex: 0});
+        //$("#dark-background").hide();
     });
 
-
-
-/*
-    $("body").on("mouseenter",".interests-picture", function() {
-        //console.log(1);
-        $(event.target).css({zIndex: 10});
-        event.stopImmediatePropagation();
-    });
-
-    $("body").on("mouseleave",".interests-picture", function() {
-        //console.log(2);
-        $(event.target).css({zIndex: 0});
-        $("#dark-background").hide();
-        event.stopImmediatePropagation();
-    });
-
-    $("body").on("mouseenter",".container-profile", function() {
-        //alert(3);
-        document.body.style.cursor="pointer";
-        $(event.target).css({zIndex: 100});
-        $("#dark-background").fadeTo(100,0.5);
-    });
-
-    $("body").on("mouseleave",".container-profile", function() {
-        //alert(4);
-        $(event.target).css({zIndex: 100});
-        document.body.style.cursor="default";
-        $("#dark-background").hide();
-    });
-*/
     $("#filter-button").on("click", function(){
         createProfileList(
             arrayPersons.filter(filterFunction))});
